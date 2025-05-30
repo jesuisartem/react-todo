69a65b6aa467d73f9dd435e54c96cb00ce103e65
jesuisartem <jesuisartem@gmail.com>
2025-05-30 13:38:43 +0500

    добавлена возможность изменения существующей todo

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import type { TodoListItem } from '../../types/TodoListItem.interface';
 import './TodoItem.css'
+import { FaPen } from "react-icons/fa";
 
 interface Props {
     item: TodoListItem;
@@ -7,16 +9,25 @@ interface Props {
 }
 
 function TodoItem({ item, onToggle }: Props) {
+    const [isEditing, setEditing] = useState(false);
     return (
     <>
       <div className="todo-item">
         <div className='todo-item__status'>
-          <input onChange={ () => onToggle({ ...item, done: !item.done }) } type='checkbox'></input>
+          <input className='status-checkbox' onChange={ () => onToggle({ ...item, done: !item.done }) } type='checkbox'></input>
         </div>
-        <div className={`
-          ${'todo-item__value'}
-          ${item.done ? 'todo-item__value_done' : ''}
-        `}>{ item.id }. { item.title }</div>
+        {
+          !isEditing 
+            ? <div className={`${'todo-item__value'} ${item.done ? 'todo-item__value_done' : ''}`}>
+                { item.title }
+              </div>
+            : <input className='todo-item__input' onChange={ (e) => onToggle({ ...item, title: e.target.value }) } type='text'></input>
+        }
+        <div className='todo-item__editing-toggle'>
+          <button onClick={ () => setEditing(() => !isEditing) } type='button'>
+            <FaPen size={ '1rem' }></FaPen>
+          </button>
+        </div>
       </div>    
     </>
   )
